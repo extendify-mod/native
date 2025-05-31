ff08e4418b035f7404ec9909772d78b739333d32
sadan <117494111+sadan4@users.noreply.github.com>
2025-05-30 21:31:48 -0400

    fix another bug in ScopedV8Context
    and format

--- src/api/util/ScopedV8Context.cpp
+++ src/api/util/ScopedV8Context.cpp
@@ -1,34 +1,38 @@
 #include "ScopedV8Context.hpp"
+
 #include "log/log.hpp"
+
 #include <cef_v8.h>
 
+
 namespace Extendify::api::util {
-	ScopedV8Context::ScopedV8Context(CefRefPtr<CefV8Context> context):
+	ScopedV8Context::ScopedV8Context(CefRefPtr<CefV8Context> _context):
 		id(nextId++),
-		context(std::move(context)) {
-			if (!this->context->IsValid()) {
-				E_ASSERT(
-						false
-						&& "trying to use ScopedV8Context with an invalid context");
-			}
-			if (CefV8Context::InContext() && CefV8Context::GetEnteredContext()->IsSame(this->context)) {
-				shouldExit = false;
-			} else {
-				context->Enter();
-			}
-			logger.trace(
-					"Entering ScopedV8Context {}, shouldExit: {}", id, shouldExit);
+		context(std::move(_context)) {
+		if (!context->IsValid()) {
+			E_ASSERT(
+				false
+				&& "trying to use ScopedV8Context with an invalid context");
 		}
+		if (CefV8Context::InContext()
+			&& CefV8Context::GetEnteredContext()->IsSame(context)) {
+			shouldExit = false;
+		} else {
+			context->Enter();
+		}
+		logger.trace(
+			"Entering ScopedV8Context {}, shouldExit: {}", id, shouldExit);
+	}
 
 	ScopedV8Context::~ScopedV8Context() {
 		if (shouldExit) {
 			context->Exit();
 		}
 		logger.trace(
-				"Exiting ScopedV8Context {}, shouldExit: {}", id, shouldExit);
+			"Exiting ScopedV8Context {}, shouldExit: {}", id, shouldExit);
 	}
 
 	log::Logger ScopedV8Context::logger {
 		{"Extendify", "api", "ScopedV8Context"}};
 	int ScopedV8Context::nextId = 1;
-}
+} // namespace Extendify::api::util
